c76c9eaccfd9fcf4931b300f24a409229df18542
Landon Poch <landon@mini.poch.localhost>
2012-03-31 15:02:18 -0500

    Some more testing around

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
-#This file is for random testing of python32 syntax/features
+#This file is for random testing of python32 syntax/feature
+import traceback
 
 class MyDate:
-  months = dict(January=31, 
-    February=28, 
-    March=31, 
-    April=30, 
-    May=31, 
-    June=30, 
-    July=31, 
-    August=31, 
-    September=30, 
-    October=31, 
-    November=30, 
-    December=31)
+  months = [('January', 31), 
+    ('February', 28), 
+    ('March', 31), 
+    ('April', 30), 
+    ('May', 31), 
+    ('June', 30), 
+    ('July', 31), 
+    ('August', 31), 
+    ('September', 30), 
+    ('October', 31), 
+    ('November', 30), 
+    ('December', 31)]
   days = ['Sunday', 
     'Monday', 
     'Tuesday', 
@@ -21,16 +22,23 @@ class MyDate:
     'Friday', 
     'Saturday']
   def __init__(self, month, day, year):
-    self.month = month
-    self.day = day
+    self.month = MyDate.months[month - 1]
+    if day <= self.month[1]:
+      self.day = day
+    else:
+      raise ValueError('Not that many days in the month')
     self.year = year
 
   def days_in_year(self):
-    print(str(sum(MyDate.months.values())))
+    return sum(month[1] for month in MyDate.months)
   
   def day_calculator(self):
     day = int(MyDate.days.index(input('Please choose a day of the week: ').capitalize()))
     daysToAdd = int(input('How many days would you like to add? '))
     print(MyDate.days[(day + daysToAdd) % 7])
-    
-date = MyDate(3, 31, 2012)
+
+try:    
+  date = MyDate(3, 31, 2012)
+  print(date.days_in_year())
+except:
+  traceback.print_exc()
