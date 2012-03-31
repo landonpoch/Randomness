943ba855b68936c6aa089365d50ab5d15d7c4580
landon.poch <landon.poch@gmail.com>
2012-03-31 12:41:38 -0500

    Updated naming conventions and added some more things

--- test.py
+++ test.py
@@ -1,34 +1,36 @@
 #This file is for random testing of python32 syntax/features
 
 class MyDate:
-  def __init__(self):
-    self.months = dict(January=31, 
-      February=28, 
-      March=31, 
-      April=30, 
-      May=31, 
-      June=30, 
-      July=31, 
-      August=31, 
-      September=30, 
-      October=31, 
-      November=30, 
-      December=31)
-    self.days = ['Sunday', 
-      'Monday', 
-      'Tuesday', 
-      'Wednesday', 
-      'Thursday', 
-      'Friday', 
-      'Saturday']
+  months = dict(January=31, 
+    February=28, 
+    March=31, 
+    April=30, 
+    May=31, 
+    June=30, 
+    July=31, 
+    August=31, 
+    September=30, 
+    October=31, 
+    November=30, 
+    December=31)
+  days = ['Sunday', 
+    'Monday', 
+    'Tuesday', 
+    'Wednesday', 
+    'Thursday', 
+    'Friday', 
+    'Saturday']
+  def __init__(self, month, day, year):
+    self.month = month
+    self.day = day
+    self.year = year
 
-  def daysInYear(self):
-    print(str(sum(self.months.values())))
+  def days_in_year(self):
+    print(str(sum(MyDate.months.values())))
   
-  def addDays(self):
-    day = int(self.days.index(input('Please choose a day of the week: ').capitalize()))
+  def day_calculator(self):
+    day = int(MyDate.days.index(input('Please choose a day of the week: ').capitalize()))
     daysToAdd = int(input('How many days would you like to add? '))
-    print(self.days[(day + daysToAdd) % 7])
+    print(MyDate.days[(day + daysToAdd) % 7])
     
-date = MyDate()
-date.daysInYear()
+date = MyDate(3, 31, 2012)
