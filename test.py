f520e619d7729c18d6ba58894b5198aa1702be10
landon.poch <landon.poch@gmail.com>
2012-03-31 11:36:42 -0500

    Class-ifying the randomness

--- test.py
+++ test.py
@@ -1,37 +1,34 @@
 #This file is for random testing of python32 syntax/features
 
-Months = dict(January=31,
-  February=28,
-  March=31,
-  April=30,
-  May=31,
-  June=30,
-  July=31,
-  August=31,
-  September=30,
-  October=31,
-  November=30,
-  December=31)
-
-Days = ['Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday']
-  
 class MyDate:
   def __init__(self):
-    pass
+    self.months = dict(January=31, 
+      February=28, 
+      March=31, 
+      April=30, 
+      May=31, 
+      June=30, 
+      July=31, 
+      August=31, 
+      September=30, 
+      October=31, 
+      November=30, 
+      December=31)
+    self.days = ['Sunday', 
+      'Monday', 
+      'Tuesday', 
+      'Wednesday', 
+      'Thursday', 
+      'Friday', 
+      'Saturday']
 
-def daysInYear():
-  print(str(sum(Months.values())))
+  def daysInYear(self):
+    print(str(sum(self.months.values())))
   
-def addDays():
-  day = int(Days.index(input('Please choose a day of the week: ').capitalize()))
-  daysToAdd = int(input('How many days would you like to add? '))
-  print(Days[(day + daysToAdd) % 7])
-  
-#addDays()
-daysInYear()
+  def addDays(self):
+    day = int(self.days.index(input('Please choose a day of the week: ').capitalize()))
+    daysToAdd = int(input('How many days would you like to add? '))
+    print(self.days[(day + daysToAdd) % 7])
+    
+date = MyDate()
+date.daysInYear()
