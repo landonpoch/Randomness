8214c7eee221d3f20dae969227622a8eaae067ef
Landon Poch <landon.poch@gmail.com>
2012-03-31 02:20:44 -0500

    Adding some random functionality

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 #This file is for random testing of python32 syntax/features
 
-print('Test!')
+Days = ['Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday']
+
+def AddDays():
+  day = int(Days.index(input('Please choose a day of the week: ').capitalize()))
+  daysToAdd = int(input('How many days would you like to add? '))
+  print(Days[(day + daysToAdd) % 7])
+  
+AddDays()
