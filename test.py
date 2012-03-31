f7310a3113bc5e3fd9d7d8eccd0c71540c6d4826
Landon Poch <landon.poch@gmail.com>
2012-03-31 02:31:36 -0500

    Adding some more random testing

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 #This file is for random testing of python32 syntax/features
 
+Months = dict(January=31,
+  February=28,
+  March=31,
+  April=30,
+  May=31,
+  June=30,
+  July=31,
+  August=31,
+  September=30,
+  October=31,
+  November=30,
+  December=31)
+
 Days = ['Sunday',
   'Monday',
   'Tuesday',
@@ -7,10 +20,18 @@ Days = ['Sunday',
   'Thursday',
   'Friday',
   'Saturday']
+  
+class MyDate:
+  def __init__(self):
+    pass
 
-def AddDays():
+def daysInYear():
+  print(str(sum(Months.values())))
+  
+def addDays():
   day = int(Days.index(input('Please choose a day of the week: ').capitalize()))
   daysToAdd = int(input('How many days would you like to add? '))
   print(Days[(day + daysToAdd) % 7])
   
-AddDays()
+#addDays()
+daysInYear()
