eaf1bfd108af9ff9551e4d2aee4e05772e66e247
landonpoch <landon.poch+git@gmail.com>
2013-07-10 01:01:31 -0500

    More random testing around streaming and throttling.

--- node/stream testing/stream-test.js
+++ node/stream testing/stream-test.js
@@ -3,16 +3,19 @@ var fs = require('fs');
 var path = require('path');
 
 var fileName = path.join(__dirname, 'lowercase.txt');
-var fileStream = fs.createReadStream(fileName);
-fileStream.on('readable', function() {
+var fileStream = fs.createReadStream(fileName, {highWaterMark: 31});
+
+fileStream.on('readable', function() {  
   var cap = new stream.Transform({decodeStrings: false});
   cap._transform = function(chunk, encoding, callback) {
     callback(null, chunk.toString().toUpperCase());
   };
   cap.on('readable', function() {
-    process.stdout.write(cap.read(10));
+    process.stdout.write(cap.read());
   });
-  cap.write(fileStream.read(10));
+  setTimeout(function() {
+    cap.write(fileStream.read() + '|');
+  }, 1000);
 });
 
 fileStream.on('error', function(err) {
