b80f36b1540e6288c09b41fa0d4a15b667910a8e
Landon Poch <landon.poch@gmail.com>
2017-10-02 22:57:45 -0600

    Testing some random stuff

--- haskell/test/src/Lib.hs
+++ haskell/test/src/Lib.hs
@@ -1,23 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Lib
-    ( bootstrap
+    ( test
     ) where
 
-import           Data.Aeson            (Value)
+import           Data.Aeson            
+import           Data.Aeson.Types      as A
 import qualified Data.ByteString.Char8 as S8
-import qualified Data.Yaml             as Yaml
+-- import qualified Data.Yaml             as Yaml
 import           Control.Monad
 import           Network.HTTP.Simple
 
-bootstrap :: String -> IO ()
-bootstrap = S8.putStrLn <=< fmap encodeResponse . httpJSON <=< parseRequest
-    -- response <- httpJSON <=< parseRequest $ val
-    -- S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+-- bootstrap :: String -> IO ()
+-- bootstrap = S8.putStrLn <=< fmap encodeResponse . httpJSON <=< parseRequest
 
-encodeResponse :: Response Value -> S8.ByteString
-encodeResponse = Yaml.encode . getResponseBody
+-- encodeResponse :: Response Value -> S8.ByteString
+-- encodeResponse = Yaml.encode . getResponseBody
 
--- someFunc :: IO ()
--- someFunc = do
---     response <- httpJSON "https://webapp.movetv.com/npv/cfdir.json"
---     S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
+test :: Response Value -> IO ()
+test response = do
+    let value = getResponseBody response
+    let json = (parseJSON :: Value -> Parser Environment) $ value
+    let tester = eitherDecode value :: Either String Environment
+    return ()
+
+data Environment = Environment
+    { configHost    :: !String
+    , configHostSsl :: !String
+    } deriving Show
+
+instance FromJSON Environment where
+    parseJSON = withObject "environment" $ \o -> do
+        configHost    <- o .: "config_host"
+        configHostSsl <- o .: "config_host_ssl"
+        return Environment{..}
