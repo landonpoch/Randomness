14f49246ccff6e416708362c9efe4994e31bd1cd
Landon Poch <landon.poch@echostar.com>
2017-10-04 22:06:42 -0600

    Separating types out to different module.

--- haskell/test/src/Lib.hs
+++ haskell/test/src/Lib.hs
@@ -1,43 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 module Lib
     ( bootstrap
     ) where
 
-import           Data.Aeson            
-import           Data.Aeson.Types      as A
-import qualified Data.ByteString.Char8 as S8
-import qualified Data.Yaml             as Yaml
 import           Control.Monad
 import           Network.HTTP.Simple
-import           Data.HashMap.Strict
+import qualified Types.Environments as T
+-- import qualified Data.ByteString.Char8 as S8
+-- import qualified Data.Yaml             as Yaml
 -- import           Text.Read             (readMaybe)
+
+type Url = String
+bootstrap :: Url -> IO (T.Environments)
+bootstrap = fmap getResponseBody . httpJSON <=< parseRequest
 
 -- bootstrap :: String -> IO ()
 -- bootstrap = S8.putStrLn <=< fmap encodeResponse . httpJSON <=< parseRequest
 
 -- encodeResponse :: Response Value -> S8.ByteString
 -- encodeResponse = Yaml.encode . getResponseBody
-
-type Url = String
-
-bootstrap :: Url -> IO (Environments)
-bootstrap = fmap getResponseBody . httpJSON <=< parseRequest
-
-data Environment = Environment
-    { configHost    :: !String
-    , configHostSsl :: Maybe String
-    } deriving Show
-
-instance FromJSON Environment where
-    parseJSON = withObject "environment" $ \o -> do
-        configHost    <- o .: "config_host"
-        configHostSsl <- o .:? "config_host_ssl"
-        return Environment{..}
-
-data Environments = Environments { environments :: HashMap String Environment } deriving Show
-
-instance FromJSON Environments where
-    parseJSON = withObject "environments" $ \o -> do
-        environments <- o .: "environments"
-        return Environments{..}
---
+++ haskell/test/src/types/Environments.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Types.Environments ( Environments ) where
+
+import Data.HashMap.Strict
+import Data.Aeson
+
+data Environment = Environment
+    { configHost    :: !String
+    , configHostSsl :: Maybe String
+    } deriving Show
+
+data Environments = Environments { environments :: HashMap String Environment } deriving Show
+
+instance FromJSON Environment where
+    parseJSON = withObject "environment" $ \o -> do
+        configHost    <- o .: "config_host"
+        configHostSsl <- o .:? "config_host_ssl"
+        return Environment{..}
+
+instance FromJSON Environments where
+    parseJSON = withObject "environments" $ \o -> do
+        environments <- o .: "environments"
+        return Environments{..}
