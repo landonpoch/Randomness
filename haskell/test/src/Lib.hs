8bfa2ee727a59b4a23a02b304bc5cb3cc6dfd83f
Landon Poch <landon.poch@echostar.com>
2017-10-09 00:19:29 -0600

    Weird abstraction.

--- haskell/test/src/Lib.hs
+++ haskell/test/src/Lib.hs
@@ -4,13 +4,16 @@ module Lib
     ) where
 
 import           Control.Monad                 ( (<=<) )
-import           Network.HTTP.Simple           ( getResponseBody, httpJSON, httpLBS, parseRequest )
+import           Network.HTTP.Simple           ( getResponseBody, httpJSON, httpLBS, parseRequest, Request, Response )
 import           Data.Aeson                    ( FromJSON )
 import qualified Data.ByteString.Lazy.Char8 as L8
 
 type Url = String
 requestJSON :: (FromJSON a) => Url -> IO (a)
-requestJSON = fmap getResponseBody . httpJSON <=< parseRequest
+requestJSON = request httpJSON
 
 printRequest :: Url -> IO ()
-printRequest = L8.putStrLn <=< fmap getResponseBody . httpLBS <=< parseRequest
+printRequest = L8.putStrLn <=< request httpLBS
+
+request :: (Request -> IO (Response a)) -> Url -> IO (a)
+request requestMechanism = fmap getResponseBody . requestMechanism <=< parseRequest
